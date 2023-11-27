d7dc329d1eed16acc1d45529dc8b26b48d22190b
KostaKostov15 <kosta.kostov15@gmail.com>
2023-11-28 01:00:51 +0200

    update AddCatch.jsx

--- client/src/components/AddCatch/AddCatch.jsx
+++ client/src/components/AddCatch/AddCatch.jsx
@@ -4,19 +4,62 @@ const AddCatch = () => {
             <div className='space-y-12'>
                 <div className='border-b border-gray-900/10 pb-12'>
                     <h2 className='text-base font-semibold leading-7 text-gray-900 text-xl'>Location Info</h2>
-                    <p className='mt-1 text-sm leading-6 text-gray-600'>Write information about the location.</p>
+                    <p className='mt-1 text-sm leading-6 text-gray-600'>Write the location of the caught fish.</p>
 
-                    <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                    <div className='mt-10 space-y-10'>
+                        <fieldset>
+                            <legend className='text-sm font-semibold leading-6 text-gray-900'>Reservoir Type *</legend>
+
+                            <div className='mt-4 space-y-4'>
+                                <div className='flex items-center gap-x-3'>
+                                    <input
+                                        id='river'
+                                        name='waterBody'
+                                        type='radio'
+                                        className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                                    />
+                                    <label
+                                        htmlFor='river'
+                                        className='block text-sm font-medium leading-6 text-gray-900'>
+                                        River
+                                    </label>
+                                </div>
+                                <div className='flex items-center gap-x-3'>
+                                    <input
+                                        id='dam'
+                                        name='waterBody'
+                                        type='radio'
+                                        className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                                    />
+                                    <label htmlFor='dam' className='block text-sm font-medium leading-6 text-gray-900'>
+                                        Dam
+                                    </label>
+                                </div>
+                                <div className='flex items-center gap-x-3'>
+                                    <input
+                                        id='pond'
+                                        name='waterBody'
+                                        type='radio'
+                                        className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
+                                    />
+                                    <label htmlFor='pond' className='block text-sm font-medium leading-6 text-gray-900'>
+                                        Pond
+                                    </label>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </div>
+
+                    <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-4'>
                         <div className='sm:col-span-2 sm:col-start-1'>
                             <label htmlFor='city' className='block text-sm font-medium leading-6 text-gray-900'>
-                                City
+                                Reservoir Name *
                             </label>
                             <div className='mt-2'>
                                 <input
                                     type='text'
                                     name='city'
                                     id='city'
-                                    autoComplete='address-level2'
                                     className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                                 />
                             </div>
@@ -24,28 +67,13 @@ const AddCatch = () => {
 
                         <div className='sm:col-span-2'>
                             <label htmlFor='region' className='block text-sm font-medium leading-6 text-gray-900'>
-                                State / Province
+                                Area / Province *
                             </label>
                             <div className='mt-2'>
                                 <input
                                     type='text'
                                     name='region'
                                     id='region'
-                                    autoComplete='address-level1'
-                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                />
-                            </div>
-                        </div>
-
-                        <div className='sm:col-span-2'>
-                            <label htmlFor='fish-weight' className='block text-sm font-medium leading-6 text-gray-900'>
-                                Weight
-                            </label>
-                            <div className='mt-2'>
-                                <input
-                                    type='number'
-                                    name='fishWeight'
-                                    id='fish-weight'
                                     className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                                 />
                             </div>
@@ -56,140 +84,75 @@ const AddCatch = () => {
                 <div className='border-b border-gray-900/10 pb-12'>
                     <h2 className='text-base font-semibold leading-7 text-gray-900 text-xl'>Catch Info</h2>
                     <p className='mt-1 text-sm leading-6 text-gray-600'>
-                        We&apos;ll always let you know about important changes, but you pick what else you want to hear
-                        about.
+                        Write some details about the fish you caught.
                     </p>
 
                     <div className='mt-10 space-y-10'>
-                        <div className='sm:col-span-3'>
-                            <label htmlFor='fishType' className='block text-sm font-medium leading-6 text-gray-900'>
-                                Fish Type *
-                            </label>
-                            <div className='mt-2'>
-                                <select
-                                    id='fishType'
-                                    name='fishType'
-                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'>
-                                    <option>---</option>
-                                    <option>Trout</option>
-                                    <option>Pike</option>
-                                    <option>Perch</option>
-                                    <option>Chub</option>
-                                    <option>Sturgeon</option>
-                                    <option>Carp</option>
-                                    <option>Crucian Carp</option>
-                                </select>
+                        <div className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                            <div className='sm:col-span-3'>
+                                <label htmlFor='fishType' className='block text-sm font-medium leading-6 text-gray-900'>
+                                    Fish Type *
+                                </label>
+                                <div className='mt-2'>
+                                    <select
+                                        id='fishType'
+                                        name='fishType'
+                                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'>
+                                        <option>---</option>
+                                        <option>Catfish</option>
+                                        <option>Pike</option>
+                                        <option>Perch</option>
+                                        <option>Trout</option>
+                                        <option>Mullet</option>
+                                        <option>Zander (Whitefish)</option>
+                                        <option>Sturgeon</option>
+                                        <option>Barbel</option>
+                                        <option>Common Bream</option>
+                                        <option>Common Carp</option>
+                                        <option>Grass Carp</option>
+                                        <option>Silver Carp</option>
+                                        <option>Crucian Carp</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className='sm:col-span-3'>
+                                <label
+                                    htmlFor='fishWeight'
+                                    className='block text-sm font-medium leading-6 text-gray-900'>
+                                    Weight [kg] *
+                                </label>
+                                <div className='mt-2'>
+                                    <input
+                                        type='number'
+                                        step={0.01}
+                                        min={0.01}
+                                        max={400}
+                                        name='fishWeight'
+                                        id='fishWeight'
+                                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6'
+                                    />
+                                </div>
                             </div>
                         </div>
 
-                        <fieldset>
-                            <legend className='text-sm font-semibold leading-6 text-gray-900'>By Email</legend>
-                            <div className='mt-6 space-y-6'>
-                                <div className='relative flex gap-x-3'>
-                                    <div className='flex h-6 items-center'>
-                                        <input
-                                            id='comments'
-                                            name='comments'
-                                            type='checkbox'
-                                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                                        />
-                                    </div>
-                                    <div className='text-sm leading-6'>
-                                        <label htmlFor='comments' className='font-medium text-gray-900'>
-                                            Comments
-                                        </label>
-                                        <p className='text-gray-500'>
-                                            Get notified when someones posts a comment on a posting.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className='relative flex gap-x-3'>
-                                    <div className='flex h-6 items-center'>
-                                        <input
-                                            id='candidates'
-                                            name='candidates'
-                                            type='checkbox'
-                                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                                        />
-                                    </div>
-                                    <div className='text-sm leading-6'>
-                                        <label htmlFor='candidates' className='font-medium text-gray-900'>
-                                            Candidates
-                                        </label>
-                                        <p className='text-gray-500'>
-                                            Get notified when a candidate applies for a job.
-                                        </p>
-                                    </div>
-                                </div>
-                                <div className='relative flex gap-x-3'>
-                                    <div className='flex h-6 items-center'>
-                                        <input
-                                            id='offers'
-                                            name='offers'
-                                            type='checkbox'
-                                            className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                                        />
-                                    </div>
-                                    <div className='text-sm leading-6'>
-                                        <label htmlFor='offers' className='font-medium text-gray-900'>
-                                            Offers
-                                        </label>
-                                        <p className='text-gray-500'>
-                                            Get notified when a candidate accepts or rejects an offer.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </fieldset>
-                        <fieldset>
-                            <legend className='text-sm font-semibold leading-6 text-gray-900'>
-                                Push Notifications
-                            </legend>
-                            <p className='mt-1 text-sm leading-6 text-gray-600'>
-                                These are delivered via SMS to your mobile phone.
+                        <div className='col-span-full'>
+                            <label htmlFor='details' className='block text-sm font-medium leading-6 text-gray-900'>
+                                Details
+                            </label>
+                            <p className='mt-3 text-sm leading-6 text-gray-600'>
+                                Write a few sentences about the catch, baits or the fishing gear used.
                             </p>
-                            <div className='mt-6 space-y-6'>
-                                <div className='flex items-center gap-x-3'>
-                                    <input
-                                        id='push-everything'
-                                        name='push-notifications'
-                                        type='radio'
-                                        className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                                    />
-                                    <label
-                                        htmlFor='push-everything'
-                                        className='block text-sm font-medium leading-6 text-gray-900'>
-                                        Everything
-                                    </label>
-                                </div>
-                                <div className='flex items-center gap-x-3'>
-                                    <input
-                                        id='push-email'
-                                        name='push-notifications'
-                                        type='radio'
-                                        className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                                    />
-                                    <label
-                                        htmlFor='push-email'
-                                        className='block text-sm font-medium leading-6 text-gray-900'>
-                                        Same as email
-                                    </label>
-                                </div>
-                                <div className='flex items-center gap-x-3'>
-                                    <input
-                                        id='push-nothing'
-                                        name='push-notifications'
-                                        type='radio'
-                                        className='h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600'
-                                    />
-                                    <label
-                                        htmlFor='push-nothing'
-                                        className='block text-sm font-medium leading-6 text-gray-900'>
-                                        No push notifications
-                                    </label>
-                                </div>
+                            <div className='mt-2'>
+                                <textarea
+                                    id='details'
+                                    name='details'
+                                    rows={3}
+                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                    defaultValue={''}
+                                />
                             </div>
-                        </fieldset>
+                        </div>
                     </div>
                 </div>
             </div>
