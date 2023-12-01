cb897bc73f5f636640b45970b0c99120e9fb6ff4
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-01 23:26:56 +0200

    Update CatchDetails.jsx

--- client/src/components/CatchDetails/CatchDetails.jsx
+++ client/src/components/CatchDetails/CatchDetails.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { MapPinIcon } from '@heroicons/react/24/outline';
+
 import { getById } from '../../services/dataService';
+import AuthContext from '../../contexts/authContext';
 
 // import { StarIcon } from '@heroicons/react/20/solid';
 
@@ -23,6 +26,7 @@ import { getById } from '../../services/dataService';
 
 export default function CatchDetails() {
     const [catchDetails, setCatchDetails] = useState({});
+    const { userId } = useContext(AuthContext);
     const { catchId } = useParams();
 
     useEffect(() => {
@@ -41,7 +45,7 @@ export default function CatchDetails() {
                 <div className='lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8'>
                     <h1 className='text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl'>
                         {catchDetails?.owner?.username}
-                        {"'s Fish"}
+                        {"'s Catch"}
                     </h1>
                 </div>
 
@@ -56,9 +60,9 @@ export default function CatchDetails() {
                     </div>
 
                     <h2 className='sr-only'>Product information</h2>
-                    <p className='text-3xl tracking-tight text-gray-900 mt-6 text-center'>
+                    <p className='text-3xl tracking-tight text-gray-900 mt-6 text-center italic'>
                         {catchDetails.fishSpecie} / {catchDetails.fishWeight}
-                        <span className='text-base uppercase italic'> kg</span>
+                        <span className='text-base uppercase'> kg</span>
                     </p>
 
                     {/* TODO: Reviews */}
@@ -81,20 +85,36 @@ export default function CatchDetails() {
                         </div>
                     </div> */}
 
-                    {}
-                    <button className='mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
-                        Edit
-                    </button>
+                    {userId === catchDetails?.owner?._id ? (
+                        <button className='mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'>
+                            Edit
+                        </button>
+                    ) : null}
                 </div>
 
                 <div className='py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6'>
                     {/* Description and details */}
 
-                    <div className='mt-8'>
+                    <div className='mt-5'>
+                        <h2 className='text-lg font-medium text-gray-900'>Location</h2>
+
+                        <div className='flex justify-start items-center mt-4 pl-2'>
+                            <h3 className='text-sm text-gray-700 pr-1'>
+                                <MapPinIcon className='h-5 w-5' />
+                            </h3>
+                            <p className='text-base font-medium text-gray-600 capitalize'>
+                                {catchDetails.reservoirName}
+                                {', '}
+                                {catchDetails.region}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='mt-10'>
                         <h2 className='text-lg font-medium text-gray-900'>Details</h2>
 
-                        <div className='mt-4 space-y-6'>
-                            <p className='text-lg text-gray-600'>{catchDetails.details}</p>
+                        <div className='mt-4 space-y-6 pl-2'>
+                            <p className='text-base font-medium text-gray-600'>{catchDetails.details}</p>
                         </div>
                     </div>
                 </div>
