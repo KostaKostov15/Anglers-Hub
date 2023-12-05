850265d7ba2e24a0e6aa7cd3672aa06036ea8976
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-06 01:31:51 +0200

    Update CatchDetails.jsx

--- client/src/components/CatchDetails/CatchDetails.jsx
+++ client/src/components/CatchDetails/CatchDetails.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -49,7 +50,7 @@ export default function CatchDetails() {
         };
 
         fetchData();
-    }, []);
+    }, [catchId]);
 
     const toggleEditModal = () => {
         setIsEditOpen((oldState) => !oldState);
