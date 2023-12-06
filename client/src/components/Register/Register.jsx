5ff2c470547b70c8cc18db789f415c463312c587
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-06 23:17:26 +0200

    Update Register.jsx

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -38,70 +38,12 @@ const Register = () => {
             <div className='mt-9 sm:mx-auto sm:w-full sm:max-w-sm'>
                 <FormProvider {...methods}>
                     <form onSubmit={(e) => e.preventDefault} noValidate autoComplete='off' className='space-y-6'>
-                        {/* <div>
-                            <label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>
-                                Email address
-                            </label>
-                            <div className='mt-2'>
-                                <input
-                                    id='email'
-                                    name='email'
-                                    type='email'
-                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                />
-                            </div>
-                        </div> */}
                         <Input {...email_validation} />
 
-                        {/* <div>
-                            <label htmlFor='username' className='block text-sm font-medium leading-6 text-gray-900'>
-                                Username
-                            </label>
-                            <div className='mt-2'>
-                                <input
-                                    id='username'
-                                    name='username'
-                                    type='text'
-                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                />
-                            </div>
-                        </div> */}
                         <Input {...username_validation} />
 
-                        {/* <div>
-                            <div className='flex items-center justify-between'>
-                                <label htmlFor='password' className='block text-sm font-medium leading-6 text-gray-900'>
-                                    Password
-                                </label>
-                            </div>
-                            <div className='mt-2'>
-                                <input
-                                    id='password'
-                                    name='password'
-                                    type='password'
-                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                />
-                            </div>
-                        </div> */}
                         <Input {...password_validation} />
 
-                        {/* <div>
-                            <div className='flex items-center justify-between'>
-                                <label
-                                    htmlFor='rePassword'
-                                    className='block text-sm font-medium leading-6 text-gray-900'>
-                                    Confirm password
-                                </label>
-                            </div>
-                            <div className='mt-2'>
-                                <input
-                                    id='rePassword'
-                                    name='rePassword'
-                                    type='password'
-                                    className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                                />
-                            </div>
-                        </div> */}
                         <Input {...rePassword_validation} />
 
                         <div>
