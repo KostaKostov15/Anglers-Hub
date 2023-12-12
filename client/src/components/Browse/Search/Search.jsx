5275ea67f2519002dc195dc924421bc4b9fe9417
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-12 23:55:38 +0200

    Update Search component styles

--- client/src/components/Browse/Search/Search.jsx
+++ client/src/components/Browse/Search/Search.jsx
@@ -30,8 +30,9 @@ export default function Search({ searchHandler }) {
                         name='category'
                         value={categoryOption}
                         onChange={changeCategoryHandler}
-                        className='block p-2.5 w-1/5 text-sm text-gray-900 bg-gray-50 rounded-s-lg border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500'>
+                        className='block w-1/4 rounded-s-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-base sm:leading-6'>
                         <option value='region'>Location</option>
+                        <option value='reservoirName'>Reservoir Name</option>
                         <option value='fishSpecie'>Fish Specie</option>
                         <option value='fishBait'>Bait</option>
                     </select>
@@ -41,14 +42,15 @@ export default function Search({ searchHandler }) {
                         name='search'
                         onChange={changeSearchHandler}
                         value={searchValue}
-                        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500'
+                        className='block w-full  border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6'
                         placeholder='Search Locations, Fish Species, Baits...'
                     />
+
                     <button
                         type='submit'
                         onClick={submitHandler}
-                        className='absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
-                        <MagnifyingGlassIcon className='w-5 h-5' />
+                        className='p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                        <MagnifyingGlassIcon className='w-6 h-6' />
                         <span className='sr-only'>Search</span>
                     </button>
                 </div>
